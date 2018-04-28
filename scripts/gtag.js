598ed983282e8f84edec36ce42be16ab1ff5c1d6
eliasdabbas <eliasdabbas@gmail.com>
2018-04-28 15:17:21 +0200

    Removes <script> tags from GA code

--- scripts/gtag.js
+++ scripts/gtag.js
@@ -1,5 +1,3 @@
-<!-- Google Analytics -->
-<script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -7,5 +5,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-1414950-14', 'auto');
 ga('send', 'pageview');
-</script>
-<!-- End Google Analytics -->
